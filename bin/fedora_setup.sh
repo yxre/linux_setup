40737508d25ea3c66490a972167af25ff848cdf5
lee <l@yxre.io>
2024-06-03 12:08:05 -0700

    improve

--- bin/fedora_setup.sh
+++ bin/fedora_setup.sh
@@ -8,4 +8,6 @@ echo "Starting Fedora setup"
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 
-sudo dnf install   https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
