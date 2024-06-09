1aea5b9420091292161b5610437de58155d2befb
yxre <l@yxre.io>
2024-06-09 22:53:21 +0700

    update obsidian config to the latest

--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -72,6 +72,8 @@ cmp.setup({
 require("obsidian").setup({
   dir = "~/Documents/drive",
   disable_frontmatter = true,
+  new_notes_location = "current_dir",
+  wiki_link_func = "use_alias_only",
 
 
   daily_notes = {
@@ -89,7 +91,6 @@ require("obsidian").setup({
     -- Where to put new notes created from completion. Valid options are
     --  * "current_dir" - put new notes in same directory as the current buffer.
     --  * "notes_subdir" - put new notes in the default notes subdirectory.
-    new_notes_location = "current_dir",
 
     -- Whether to add the output of the node_id_func to new notes in autocompletion.
     -- E.g. "[[Foo" completes to "[[foo|Foo]]" assuming "foo" is the ID of the note.
